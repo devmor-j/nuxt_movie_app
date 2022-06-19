229bd59e68eb44010197b41ac03caa7e07111997
Morteza Jamshidi <devmor.j@gmail.com>
2022-06-19 18:16:02 +0430

    create nuxt app

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
+
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container fluid>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>

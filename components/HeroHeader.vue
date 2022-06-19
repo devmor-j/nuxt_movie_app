423051c7ea03a3cd502ceca5520b46f5a9871107
Morteza Jamshidi <devmor.j@gmail.com>
2022-06-19 18:59:33 +0430

    abstract hero header

---
+++ components/HeroHeader.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  name: 'HeroHeader',
+}
+</script>
+
+<template>
+  <v-row class="background-image" align="end">
+    <v-col class="px-0">
+      <v-container>
+        <h2 class="text-h2 pb-4">Now Streaming</h2>
+        <v-btn class="secondary--text" text outlined>Explore</v-btn>
+      </v-container>
+    </v-col>
+  </v-row>
+</template>
+
+<style scoped>
+.background-image {
+  background-image: linear-gradient(to top, rgb(0 0 0 / 0.5), transparent), url("/images/popcorn.jpg");
+  min-height: 35vh;
+  background-repeat: no-repeat;
+  background-position: 50% 30%;
+  background-size: cover;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,4 +5,7 @@ export default {
 </script>
 
 <template>
+  <div>
+    <HeroHeader />
+  </div>
 </template>

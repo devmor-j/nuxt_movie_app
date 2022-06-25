1ce9d441d1f934b3d086412215e5e6e3c8589e9b
Morteza Jamshidi <devmor.j@gmail.com>
2022-06-25 22:58:48 +0430

    config nuxt for client side rendering

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,7 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   target: 'static',
+  ssr: false,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - nuxt_movie_app',
